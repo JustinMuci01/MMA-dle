5c69d74f3a9b3e4c70e67fb0d8f865c3d3630c01
JustinMuci01 <justinmuci@gmail.com>
2025-10-07 20:52:03 -0500

    changed title

--- Python Fetch/index.py
+++ Python Fetch/index.py
@@ -40,24 +40,62 @@ def makeJsonString(fighter):
     }
     return fighter_dict
     
+
+def getFighter(fighterName):
+    try:
+        conn = sqlite3.connect(f"{script_directory}/datafiles/ufc_database.db")
+        dbcursor = conn.cursor()
+
+        sql = """SELECT * FROM Fighters
+        WHERE Name = ?"""
+        dbcursor.execute(sql, fighterName)
+        result = dbcursor.fetchone()
+    except sqlite3.IntegrityError as e:
+        print(f"Database Integrity Error {e}")
+        conn.rollback()
+    except sqlite3.Error as e:
+        print(f"Database Error: {e}")
+        conn.rollback()
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        conn.rollback()
+    finally:
+        if (result):
+            for row in result:
+                myFighter = Fighter(row[0], row[5], row[2], row[3], row[4], row[6], row[1], row[7])
+                return makeJsonString(myFighter)
+
     
-myArray = []
-conn = sqlite3.connect(f"{script_directory}/datafiles/ufc_database.db")
-dbcursor = conn.cursor()
-sql = "SELECT * FROM Fighters"
-dbcursor.execute(sql)
-result = dbcursor.fetchall()
+#Build whole Fighter Array
+try:
+    myArray = []
+    conn = sqlite3.connect(f"{script_directory}/datafiles/ufc_database.db")
+    dbcursor = conn.cursor()
+    sql = "SELECT * FROM Fighters"
+    dbcursor.execute(sql)
+    result = dbcursor.fetchall()
 
-for row in result:
-    newFighter = Fighter(row[0], row[5], row[2], row[3], row[4], row[6], row[1], row[7])
-    myArray.append(newFighter) 
+except sqlite3.IntegrityError as e:
+    print(f"Database Integrity Error {e}")
+    conn.rollback()
+except sqlite3.Error as e:
+    print(f"Database Error: {e}")
+    conn.rollback()
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+    conn.rollback()
 
-jsonFighterList = []
-for fighter in myArray:
-    jsonFighterList.append(makeJsonString(fighter))
+finally:
+    for row in result:
+        newFighter = Fighter(row[0], row[5], row[2], row[3], row[4], row[6], row[1], row[7])
+        myArray.append(newFighter) 
 
-json_string = json.dumps(jsonFighterList, indent=4)
-print(json_string)
+    jsonFighterList = []
+    for fighter in myArray:
+        jsonFighterList.append(makeJsonString(fighter))
 
-with open(f"{script_directory}/../src/fighters.js", "w") as f:
-    f.write(f"const FighterList = {json_string}")
+    json_string = json.dumps(jsonFighterList, indent=4)
+    print(json_string)
+
+    with open(f"{script_directory}/../src/fighters.js", "w") as f:
+        f.write(f"const FighterList = {json_string}")
--- src/MMATable.jsx
+++ src/MMATable.jsx
@@ -1,13 +1,37 @@
 import React, {useState, useEffect} from "react";
 import { FighterList } from "./fighters";
+import './index.css'
 
 function MMATable(props)
 {
-    
+    const[targetFighter, setTargetFighter] = useState(FighterList[0]);
+    const[guessCount, setGuessCount] = useState(0);
+    const weightClasses = ["Flw", "BW", "FW", "LW", "WW", "MW", "LHW", "HW"];
 
     return(
         <>
-        <button onClick = {() => buildTable()}> Hello </button>        
+        <head>
+            <link rel="icon" href="../img/gloves.png" type = "image"/>
+            <title>MMA-DLE</title>
+        </head>
+        <body>
+        
+        <div className = "headerBar">
+            <input type="text" className = "searchBar" placeholder="Type a guess here..."/>
+        </div>
+        <div className = "fighterCard"> 
+            <div className = "topRow">
+            <div className = "fighterInfo"> {targetFighter.country}</div>
+            <img loading = "lazy" alt="" className = "fighterImg"/>
+            <div className = "fighterInfo"> {targetFighter.weightClass}</div>
+            </div>
+            <div className = "bottomRow">
+            <div className = "fighterInfo"> {targetFighter.wins}</div>
+            <div className = "fighterInfo"> {targetFighter.losses}</div>
+            <div className = "fighterInfo"> {targetFighter.draws}</div>
+            </div>
+        </div>   
+        </body>       
         </>
     );
 }
