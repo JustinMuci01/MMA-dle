c89781d6c9aa1625fd536a8cc28ef884090200b2
JustinMuci01 <justinmuci@gmail.com>
2025-12-13 14:19:57 -0600

    downloaded mysql

--- Python Fetch/test.py
+++ Python Fetch/test.py
@@ -1,16 +1,58 @@
 from bs4 import BeautifulSoup
+import  mysql.connector
 import requests
 
-url = "https://www.ufc.com/rankings"
 
-page = requests.get(url)
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user="JMuci14",
+    password="Qwepoi_20128059",
+    database = "mmafighters"
+)
 
-soup = BeautifulSoup(page.text, 'html.parser')
+mycursor = mydb.cursor()
 
-soup.find('table')
-string_list = soup.find_all('h5')
-for i in range(13):
-    print(string_list[i].text)
+def showDataBases():
+    mycursor.execute("SHOW DATABASES")
+    for x in mycursor:
+        print(x)
+    return
 
-# with open("test.html", 'w', encoding = 'utf-8') as page:
-#     page.write(html_string)
+
+def createBase():
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS MMAFighters")
+
+def createTable():
+    mycursor.execute("""CREATE TABLE IF NOT EXISTS Fighters ( 
+                    Name VARCHAR(255) PRIMARY KEY,
+                    Ranking INT,
+                    Wins INT,
+                    Losses INT,
+                    Draws INT,
+                    WeightClass VARCHAR(10),
+                    Country VARCHAR(255),
+                    Pic VARCHAR(1000)
+                     )""")
+
+
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+    print(x)
+
+# def scanFighter():    
+#     print('hi')
+#     return
+
+# url = "https://www.ufc.com/rankings"
+
+# page = requests.get(url)
+
+# soup = BeautifulSoup(page.text, 'html.parser')
+
+# soup.find('table')
+# string_list = soup.find(attrs = {"class: "})
+# print(string_list)
+
+# # with open("test.html", 'w', encoding = 'utf-8') as page:
+# #     page.write(html_string)
